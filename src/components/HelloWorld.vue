0b133547dad812aa5335b3d606b43c6023424ddb
nagashi_ma_w <nagashima5.wataru@gmail.com>
2019-01-26 15:42:25 +0900

    サンプル元からhtml/cssをコピペ(なんもわからんので)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,60 @@
 <template>
+  <div>
+    {{ msg }}
+    <form>
+      <button>add task</button>
+      <button>delete finished task</button>
+      <p>input: <input type="text"></p>
+      <p>task</p>
+    </form>
+    <div class="task-list">
+      <label class="task-list__item"><input type="checkbox"><button>edit</button>vue-router</label>
+      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vuex</label>
+      <label class="task-list__item"><input type="checkbox"><button>EDIT</button>vue-loader</label>
+      <label class="task-list__item--checked"><input type="checkbox" checked><button>EDIT</button>awesome-vue</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@mixin flex-vender() {
+  display: flex;
+  display: -webkit-flex;
+  display: -moz-flex;
+  display: -ms-flex;
+  display: -o-flex;
+}
+.task-list {
+  @include flex-vender;
+  flex-direction: column;
+  align-items: center;
+  &__item {
+    width: 270px;
+    text-align: left;
+    $element: #{&};
+    &--checked {
+      @extend #{$element};
+      color: #85a6c6;
+    }
+  }
+}
+</style>
+
+
+<!--
+<template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
@@ -111,3 +167,4 @@ a {
   color: #42b983;
 }
 </style>
+-->
